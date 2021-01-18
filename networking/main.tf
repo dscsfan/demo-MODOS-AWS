a27289c9a9203722bbf0b39877f79bf0d0751de7
Song Fan <song.fan@exlservice.com>
2021-01-18 18:54:15 +0000

    added main.tf vars.tf in networking folder

---
+++ networking/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "netwk" {
+  source          = "app.terraform.io/DatasourceConsulting/netwk/aws"
+  azs             = var.azs
+  igw_name        = var.igw_name
+  pri_snet_cidr_1 = var.pri_snet_cidr_1
+  pri_snet_cidr_2 = var.pri_snet_cidr_2
+  pri_snet_name_1 = var.pri_snet_name_1
+  pri_snet_name_2 = var.pri_snet_name_2
+  project_name    = var.project_name
+  pub_snet_cidr   = var.pub_snet_cidr
+  pub_snet_name   = var.pub_snet_name
+  rt_name_pub     = var.rt_name_pub
+  vpc_cidr        = var.vpc_cidr
+  vpc_name        = var.vpc_name
+  version         = "1.0.0"
+}
---
+++ networking/vars.tf
@@ -0,0 +1,52 @@
+variable "AWS_REGION" {
+  description = "AWS Region"
+  #default = "us-west-1"
+}
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  #default = "10.1.0.0/16"
+}
+variable "pub_snet_cidr" {
+  description = "public subnet CIDR Block"
+  #default = "10.1.1.0/24"
+}
+variable "pri_snet_cidr_1" {
+  description = "private subnet 1 CIDR block"
+  #default = "10.1.2.0/24"
+}
+variable "pri_snet_cidr_2" {
+  description = "private subnet 2 CIDR block"
+  #default = "10.1.3.0/24"
+}
+variable "vpc_name" {
+  description = "VPC Name"
+  #default = "vpc-modos-aws"
+}
+variable "pub_snet_name" {
+  description = "public subnet name"
+  #default = "snet-pub-modos-aws-1"
+}
+variable "pri_snet_name_1" {
+  description = "private subnet 1 name"
+  #default = "snet-pri-modos-aws-1"
+}
+variable "pri_snet_name_2" {
+  description = "private subnet 2 name"
+  #default = "snet-pri-modos-aws-2"
+}
+variable "project_name" {
+  description = "Project Name"
+  #default = "modos-aws"
+}
+variable "azs" {
+  description = "availability zones"
+  #default = [ "us-west-1a", "us-west-1c", "us-west-1c"]
+}
+variable "igw_name" {
+  description = "internet gateway name"
+  #default = "igw-modos-aws"
+}
+variable "rt_name_pub" {
+  description = "route table name"
+  #default = "rt-MODOS-AWS-pub-1"
+}
