abda9c5252ac5dbbfd1a354ef9ff87a86faf4605
Song Fan <song.fan@exlservice.com>
2021-01-19 04:24:39 +0000

    added more variables

--- rds/database.tf
+++ rds/database.tf
@@ -18,8 +18,8 @@ module "db" {
   apply_immediately       = true
   monitoring_interval     = 10
 
-  db_parameter_group_name         = "default"
-  db_cluster_parameter_group_name = "default"
+  #db_parameter_group_name         = "default"
+  #db_cluster_parameter_group_name = "default"
 
   #enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
 
