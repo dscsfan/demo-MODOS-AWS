400de890feebd6de344037f74d9dd5a50a1ef4c7
Song Fan <song.fan@exlservice.com>
2021-01-22 22:59:00 +0000

    added kinesis

---
+++ kinesis/vars.tf
@@ -0,0 +1,15 @@
+variable "AWS_REGION" {
+  default = "us-west-1"
+}
+variable "kinesis_name" {
+  description = "Kinesis Stream Name"
+}
+variable "project_name" {
+  description = "Project Name"
+}
+variable "retention_period" {
+  description = "Retention Period"
+}
+variable "shard_count" {
+  description = "Shard Count"
+}
