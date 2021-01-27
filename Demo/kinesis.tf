3f25d130be935ff3a7e2f6cf7a1448efe2747576
Song Fan <song.fan@exlservice.com>
2021-01-27 17:35:12 +0000

    added s3

--- Demo/kinesis.tf
+++ Demo/kinesis.tf
@@ -8,3 +8,12 @@ module "kinesis" {
   retention_period = var.retention_period
   shard_count      = var.shard_count
 }
+
+resource "aws_s3_bucket" "my_s3" {
+  bucket = "modos-aws-demo-123"
+  acl    = "private"
+  tags = {
+    Project  = var.project_name
+    Name = "modos-aws-demo-123"
+  }
+}
