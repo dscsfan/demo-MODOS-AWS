59c2763c4b30c0e2c42b750a48dff26bb971e1b3
Song Fan <song.fan@exlservice.com>
2021-01-28 00:43:17 +0000

    added Demo

---
+++ Demo/main.tf
@@ -0,0 +1,10 @@
+module "kinesis" {
+  source  = "app.terraform.io/DatasourceConsulting/kinesis/aws"
+  version = "1.0.2"
+  # insert required variables here
+  AWS_REGION       = var.AWS_REGION
+  kinesis_name     = var.kinesis_name
+  project_name     = var.project_name
+  retention_period = var.retention_period
+  shard_count      = var.shard_count
+}
---
+++ Demo/vars.tf
@@ -0,0 +1,5 @@
+variable "AWS_REGION" {}
+variable "kinesis_name" {}
+variable "project_name" {}
+variable "retention_period" {}
+variable "shard_count" {}
