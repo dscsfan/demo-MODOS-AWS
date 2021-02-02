9666eeeb2626c468e23cdf21f16b0ed620af8735
Song Fan <song.fan@exlservice.com>
2021-02-02 01:51:27 +0000

    added glue table

---
+++ glue/glue.tf
@@ -0,0 +1,40 @@
+data "aws_iam_role" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.glue_role_name]
+  }
+}
+
+resource "aws_glue_catalog_database" "catalog_database" {
+  name = var.catalog_db_name
+}
+
+resource "aws_glue_catalog_table" "aws_glue_catalog_table" {
+  name          = var.catalog_table_name
+  database_name = aws_glue_catalog_database.catalog_database.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    parameters = {
+      endpointUrl = "https://kinesis.us-west-1.amazonaws.com",
+      streamName  = "ks-modos-aws-1",
+      typeOfData  = "kinesis"
+    }
+    columns {
+      name = "sensorid"
+      type = "string"
+    }
+
+    columns {
+      name = "currenttemperature"
+      type = "string"
+    }
+
+    columns {
+      name = "status"
+      type = "string"
+    }
+  }
+}
+
---
+++ glue/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_REGION" {
+  default = "us-west-1"
+}
+variable "modos_aws_glue_role" {
+
+}
+variable "glue_role_name" {
+  default = "modos-aws-glue-role"
+}
+
+variable "catalog_db_name" {
+  
+}
+variable "catalog_table_name" {
+  
+}
