a1032daec20ec29ba82f221927c1a3bd309df6e9
Song Fan <song.fan@exlservice.com>
2021-02-06 00:58:12 +0000

    modified glue table

--- glue/glue.tf
+++ glue/glue.tf
@@ -11,13 +11,27 @@ resource "aws_glue_catalog_table" "aws_glue_catalog_table" {
   database_name = aws_glue_catalog_database.catalog_database.name
 
   table_type = "EXTERNAL_TABLE"
-
+  
   storage_descriptor {
+    input_format = "org.apache.hadoop.mapred.TextInputFormat"
+    location = "ks-modos-aws-1"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
     parameters = {
-      endpointUrl = "https://kinesis.us-west-1.amazonaws.com",
-      streamName  = "ks-modos-aws-1",
+      endpointUrl = "https://kinesis.us-west-1.amazonaws.com"
+      streamName  = "ks-modos-aws-1"
       typeOfData  = "kinesis"
     }
+
+    ser_de_info {
+      name                  = "my-stream"
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+
     columns {
       name = "sensorid"
       type = "number"
