4f40a79725d7066726c0c01def2d96f79c2a2749
Song Fan <song.fan@exlservice.com>
2021-02-09 00:06:51 +0000

    added glue_s3_curated

---
+++ glue_s3_curated/vars.tf
@@ -0,0 +1,15 @@
+variable "AWS_REGION" {
+  
+}
+variable "catalog_db_s3_curated_name" {
+  
+}
+variable "crawler_s3_curated_name" {
+  
+}
+variable "glue_role_name" {
+  
+}
+variable "s3_curated_bucket_name" {
+  
+}
