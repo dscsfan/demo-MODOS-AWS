f85032ccad415e2c943a2873afc4e63f0a5ee86a
Song Fan <song.fan@exlservice.com>
2021-02-11 00:55:37 +0000

    added cloudtrail

--- cloudtrail/cloudtrail.tf
+++ cloudtrail/cloudtrail.tf
@@ -45,7 +45,7 @@ resource "aws_s3_bucket" "mys3" {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::${var.s3_bucket_name}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Resource": "arn:aws:s3:::${var.s3_bucket_name}/cloudtrail/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
             "Condition": {
                 "StringEquals": {
                     "s3:x-amz-acl": "bucket-owner-full-control"
