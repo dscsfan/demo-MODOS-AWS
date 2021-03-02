6ef4b22281180cb0dc7431aaea96508e140baa58
Song Fan <song.fan@exlservice.com>
2021-03-02 00:09:39 +0000

    change module version

--- kinesis/kinesis_stream.tf
+++ kinesis/kinesis_stream.tf
@@ -1,7 +1,7 @@
 module "kinesis" {
   source  = "app.terraform.io/DatasourceConsulting/kinesis/aws"
   version = "1.0.2"
-  # insert required variables here
+  #insert required variables here
   AWS_REGION       = var.AWS_REGION
   kinesis_name     = var.kinesis_name
   project_name     = var.project_name
